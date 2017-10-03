2204a914703c0fdf5952c52f8ee2719bc69f75ea
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2017-10-03 16:23:49 +0100

    [const] Add constants file: add arcsec

---
+++ uvplot/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+arcsec = 4.84813681109536e-06       # radians
+
