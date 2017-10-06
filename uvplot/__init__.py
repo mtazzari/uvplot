f98e521645f1024d7ad9015547c172cc73bd9bae
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2017-10-06 10:57:58 +0100

    [init] Set backend to TkAgg if on macosx. Should Fix #14

--- uvplot/__init__.py
+++ uvplot/__init__.py
@@ -1,5 +1,19 @@
+
 import matplotlib
-matplotlib.use('TkAgg')
+
+# To avoid Runtime Error
+# RuntimeError: Python is not installed as a framework. The Mac OS X backend
+# will not be able to function correctly if Python is not installed as a framework.
+# See the Python documentation for more information on installing Python as a
+# framework on Mac OS X. Please either reinstall Python as a framework, or try
+# one of the other backends.
+# If you are using (Ana)Conda please install python.app and replace the use of
+# 'python' with 'pythonw'.
+# See 'Working with Matplotlib on OSX' in the Matplotlib FAQ for more information.
+# https://matplotlib.org/faq/osx_framework.html
+
+if matplotlib.get_backend().lower() == 'macosx':
+    matplotlib.use('TkAgg')
 
 from .uvtable import UVTable
 from .constants import arcsec
