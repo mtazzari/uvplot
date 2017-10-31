553311dff634127e7bd45167e3514b425edafaeb
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2017-10-31 10:36:57 +0000

    [v] Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="uvplot",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     author="Marco Tazzari",
     author_email="mtazzari@ast.cam.ac.uk",
