3afa66a8ad3a6d1b1423822d40fc0fb5e0ca27d6
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2019-09-04 13:15:34 +0100

    [setup] Ensure backward compat with Py 2.7

--- setup.py
+++ setup.py
@@ -5,6 +5,19 @@ from setuptools import setup
 # read version number
 exec(open("uvplot/_version.py", "r").read())
 
+# use README as long_description (for PyPI)
+try:
+    long_description = open("README.rst", "r", encoding="utf-8").read()
+except TypeError:
+    # under Python 2.7 the `encoding` keyword doesn't exist.
+    print(
+        "DEPRECATION: Python 2.7 will reach the end of its life on January 1st, 2020. "
+        "Please upgrade your Python as Python 2.7 won't be maintained after that date. "
+        "A future version of uvplot will drop support for Python 2.7. "
+    )
+    long_description = open("README.rst", "r").read()
+
+
 setup(
     name="uvplot",
     version=__version__,
@@ -12,7 +25,7 @@ setup(
     author="Marco Tazzari",
     author_email="mtazzari@gmail.com",
     description="Utilities for handling and plotting interferometric visibilities.",
-    long_description=open("README.rst", "r", encoding="utf-8").read(),
+    long_description=long_description,
     install_requires=["numpy>=1.9", "matplotlib"],
     license="LGPLv3",
     url="https://github.com/mtazzari/uvplot",
